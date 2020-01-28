a01973ea2d77b06a63382730b1c03cacf12bda0a
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-01-29 06:56:27 +0900

    Chore

--- src/util.cpp
+++ src/util.cpp
@@ -64,7 +64,7 @@ HRESULT GetProcessName(DWORD processId, wchar_t **processName,
   while (hasProcessEntry) {
     if (processEntry.th32ProcessID == processId) {
       wchar_t *buffer = new wchar_t[256]{};
-      StringCbPrintfW(buffer, 511, L"ProcessName is %s", );
+      StringCbPrintfW(buffer, 511, L"ProcessName is %s", processEntry.szExeFile);
       Log->Info(buffer, GetCurrentThreadId(), __LONGFILE__);
       delete[] buffer;
       buffer = nullptr;
