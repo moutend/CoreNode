dde8c258e0d8a77e7e4f4a542c27d2895d3839fd
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-01-29 09:24:29 +0900

    Chore

--- src/util.cpp
+++ src/util.cpp
@@ -64,6 +64,7 @@ HRESULT GetProcessName(DWORD processId, wchar_t **processName,
   while (hasProcessEntry) {
     if (processEntry.th32ProcessID == processId) {
       size_t len = std::wcslen(processEntry.szExeFile);
+      *processNameLength = len;
       wchar_t *buffer = new wchar_t[256]{};
       StringCbPrintfW(buffer, 511, L"ProcessName is %s %d",
                       processEntry.szExeFile, len);
