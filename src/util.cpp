1d01bbdeb25715366aff204fb3baa60544970278
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-01-29 09:44:42 +0900

    Chore

--- src/util.cpp
+++ src/util.cpp
@@ -65,6 +65,7 @@ HRESULT GetProcessName(DWORD processId, wchar_t **processName,
     if (processEntry.th32ProcessID == processId) {
       size_t length = std::wcslen(processEntry.szExeFile);
       *processNameLength = length;
+      *processName = new wchar_t[length + 1]{};
       wchar_t *buffer = new wchar_t[256]{};
       StringCbPrintfW(buffer, 511, L"ProcessName is %s %d %d",
                       processEntry.szExeFile, length,
@@ -81,7 +82,7 @@ HRESULT GetProcessName(DWORD processId, wchar_t **processName,
   }
 
   SafeCloseHandle(&hSnapshot);
-return E_FAIL;
+  return E_FAIL;
 
   return S_OK;
 }
